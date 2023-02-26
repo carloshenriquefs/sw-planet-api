abac44939623849ab942e27ef1b278d860b54c32
Carlos Henrique <ichfs9108@gmail.com>
2023-02-26 18:05:24 -0300

    Testando o cenario de sucesso e dados invalidos no repository

---
+++ src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.example.swplanetapi.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.example.swplanetapi.common.PlanetConstants;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PlanetConstants.PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PlanetConstants.PLANET.getName());
+        assertThat(sut.getTerrain()).isEqualTo(PlanetConstants.PLANET.getTerrain());
+        assertThat(sut.getClimate()).isEqualTo(PlanetConstants.PLANET.getClimate());
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet("", "", "");
+
+        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
+    }
+
+}
