4788d8507bce6641af0c0a53e3ccf55dd82f6524
Carlos Henrique <ichfs9108@gmail.com>
2023-02-26 22:49:08 -0300

    Testando o cenário de planetas existente no repositorio

--- src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -40,4 +40,13 @@ public class PlanetRepositoryTest {
         assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    public void createPlanet_WithExistingName_ThrowsException() {
+        Planet planet = testEntityManager.persistFlushFind(PlanetConstants.PLANET);
+        testEntityManager.detach(planet);
+        planet.setId(null);
+
+        assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+    }
+
 }
